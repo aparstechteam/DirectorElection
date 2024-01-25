e3ebf0380ed454402a12a4e88efc5805b24765d9
nasimulhasan <nasimulhasan@gmail.com>
2024-01-25 18:51:32 +0530

    Updates

---
+++ assets/auth.996500c7.js
@@ -0,0 +1,1 @@
+import{U as t,c as o,s}from"./entry.cc468178.js";const u=t((a,c)=>{const e=useUserStore();if(o(()=>e.access_token).value===null)return s("/login")});export{u as default};
---
+++ assets/guest.10c4190b.js
@@ -0,0 +1,1 @@
+import{U as s,c as t,s as o}from"./entry.cc468178.js";const u=s((a,c)=>{const e=useUserStore();if(t(()=>e.access_token).value)return o("/")});export{u as default};
